9196e0fffe0211313b7c053460b874963a238037
Yang LI <406050086@qq.com>
2019-12-03 16:02:38 +0900

    start to study about the local sparse dataset for soil distribution error

---
+++ Local modeling project/Try_density_7_GP.m
@@ -0,0 +1,73 @@
+%% GP - Try the density of data
+% Author: LI, Yang
+% Date: Dec 23rd, 2019
+close all
+clear
+
+%% load the dataset
+load local_sparse_7_data.mat
+% X_data, Y_data, X_test inside
+
+% Grid information
+% X_local, Y_local inside
+[m,n] = size(X_local);
+
+%% Gaussian Process
+
+N = 200;  % iteration times limitation
+
+% Specify the mean, cov, likelihood
+meanfunc = [];                    % empty: don't use a mean function
+covfunc = {@covSEard};              % ARD SE
+likfunc = {@likGauss};              % Gaussian likelihood
+infmethod = @infGaussLik;               % inference with Guassian Likelihood
+% The hyperparameter struct
+hyp_init = struct('mean', [], 'cov', [0 0 0], 'lik', -1);
+    
+% Optimization hyperparameters  - exact GP
+tic;
+hyp_exactGP = minimize(hyp_init, @gp, -N, infmethod, meanfunc, covfunc,...
+    likfunc, X_data, Y_data);
+Time_of_ExactGP = toc;
+
+% The dense prediction
+[mu, s2] = gp(hyp_exactGP, infmethod, meanfunc, covfunc, likfunc,...
+    X_data, Y_data, X_test);  % Xtrain,Ytrain,Xtest
+
+% reshape the result
+H_local_exactGP = reshape(mu,[m,n]);
+
+%% Sparse Gaussian Process
+
+% inducing points
+% mid = floor(length(X_test)/2);
+% xu = X_test(mid-3:mid+3); % (1) bad choice of initial inducing set
+xu = X_test(1:25:end,:); % (2) generous choice of initial inducing set
+cov = {'apxSparse', covfunc, xu};
+inff = @(varargin) infmethod(varargin{:},struct('s', 0));
+% VFE, opt.s = 0; SPEP, 0 <opt.s < 1; FITC, opt.s = 1
+
+tic; hyp_init.xu = xu;
+hyp_sparseGP = minimize(hyp_init, @gp, -N, inff, meanfunc, cov, likfunc,...
+    X_data, Y_data);
+Time_of_SparseGP = toc;
+
+[ymu,ys2] = gp(hyp_sparseGP, inff, meanfunc, cov, likfunc,...
+    X_data, Y_data, X_test);
+
+% reshape the result
+H_local_sparse = reshape(ymu,[m,n]);
+
+% % Marginal likelihood and derivatives
+% [nlZ,dnlZ] = gp(hyp_sparseGP,infmethod, meanfunc, cov,...
+%     likfunc, X_data, Y_data);
+
+%% Plot
+Time_of_ExactGP
+Time_of_SparseGP
+diff_ExactAndSparse = immse(mu,ymu)
+
+figure
+mesh(X_local.*170, Y_local.*80, H_local_exactGP)
+figure
+mesh(X_local.*170, Y_local.*80, H_local_sparse)
---
+++ Local modeling project/Try_density_7_data.m
@@ -0,0 +1,138 @@
+%% Try the density of data
+% Author: LI, Yang
+% Date: Dec 2nd, 2019
+close all
+clear
+
+%% 1. Load the 7 data
+load('data_seven_centerdep/Error_data1011.mat')
+H = H_error;
+clear H_error
+m = length(H(:,1,1));
+n = length(H(1,:,1));
+number = length(H(1,1,:));
+
+H_full = H; % Full dataset
+
+
+%% 2. Make the data local
+load C:\Users\LI\Desktop\Approx_Surface\Vessel_XY.mat
+
+Xc = 85; Yc = 0;
+lx = 40; ly = 40;
+
+% Get the area of local space
+X_range = [Xc - lx; Xc + lx]; Y_range = [Yc - ly; Yc + ly];
+Local_area = ones(m,n);
+% represent with logical value
+X_area = X(1,:)<X_range(1); X_area = X_area | X(1,:)>X_range(2);
+Y_area = Y(:,1)<Y_range(1); Y_area = Y_area | Y(:,1)>Y_range(2);
+% convert logical value to double
+for i = 1:length(Y_area)
+    for j = 1:length(X_area)  
+        if X_area(j) == 0 && Y_area(i) == 0
+            Local_area(i,j) = 1;
+        else
+            Local_area(i,j) = 0;
+        end
+    end
+end
+
+% extract the local data from full data
+for k = 1:number
+AA = Local_area .* H_full(:,:,k);
+BB = AA;
+
+for i = length(Y_area):-1:1
+   if  Y_area(i) == 1
+       BB(i,:) = [];
+   end
+end
+for i = length(X_area):-1:1
+   if  X_area(i) == 1
+       BB(:,i) = [];
+   end
+end
+
+% figure
+% mesh(BB)
+% title("No."+k)
+
+H_local(:,:,k) = BB;
+end
+
+% extract for X and Y
+X_local = Local_area .* X;
+Y_local = Local_area .* Y;
+
+for i = length(Y_area):-1:1
+   if  Y_area(i) == 1
+       X_local(i,:) = [];
+       Y_local(i,:) = [];
+   end
+end
+for i = length(X_area):-1:1
+   if  X_area(i) == 1
+       X_local(:,i) = [];
+       Y_local(:,i) = [];
+   end
+end
+
+figure
+title("The dataset point(red) and selected point(black)")
+hold on
+plot(X,Y,'LineStyle','none','Marker','.','Color','[0.8500, 0.3250, 0.0980]')
+plot(Xc,Yc,'LineStyle','none','Marker','x','Color','[0, 0, 0]','LineWidth',1.7)
+
+plot([X_range(1),X_range(2)],[Y_range(1),Y_range(1)],'Color','[0, 0, 0]')
+plot([X_range(1),X_range(2)],[Y_range(2),Y_range(2)],'Color','[0, 0, 0]')
+plot([X_range(1),X_range(1)],[Y_range(1),Y_range(2)],'Color','[0, 0, 0]')
+plot([X_range(2),X_range(2)],[Y_range(1),Y_range(2)],'Color','[0, 0, 0]')
+
+%% 3. Make it sparse
+step_length = 9; % level of sparseness
+
+H_local_sparse = [];
+X_local_sparse = [];
+Y_local_sparse = [];
+
+[mm,nn,kk] = size(H_local);
+ix = 1; jy = 1;
+
+for i = 1:step_length:mm
+    for j = 1:step_length:nn
+        H_local_sparse(ix,jy,:) = H_local(i,j,:);
+        X_local_sparse(ix,jy) = X_local(i,j);
+        Y_local_sparse(ix,jy) = Y_local(i,j);
+        jy = jy + 1;
+    end
+    ix = ix + 1;
+    jy = 1;
+end
+
+plot(X_local_sparse,Y_local_sparse,'LineStyle','none','Marker','x','Color','[0, 0, 0]')
+
+% figure
+% mesh(X_local,Y_local,H_local(:,:,1))
+
+%% 4. Make it a data set
+
+% normalize the X = [0,1] and Y = [-1,1]
+X_local_sparse = X_local_sparse./170;
+Y_local_sparse = Y_local_sparse./80;
+
+% Matrices to verctors
+X_data_base = [X_local_sparse(:),Y_local_sparse(:)];
+X_data = [];
+for i = 1:number
+   X_data = [X_data;X_data_base]; 
+end
+
+Y_data = H_local_sparse(:);
+
+X_local = X_local./170;
+Y_local = Y_local./80;
+X_test = [X_local(:),Y_local(:)];
+
+save local_sparse_7_data X_data Y_data X_test X_local Y_local
+disp("The dataset size is N=" + length(X_data(:,1)) + " with D=" + length(X_data(:,2)))
