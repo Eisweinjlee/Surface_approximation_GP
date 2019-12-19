5f9bfcc3a3d64b37d253ca93580389de598bd0ca
Yang LI <406050086@qq.com>
2019-12-19 18:37:02 +0900

    the comparison can be made between having wall effect and not.

--- unit-symmetric Gpdf/unit_sym_input_2d.m
+++ unit-symmetric Gpdf/unit_sym_input_2d.m
@@ -11,7 +11,7 @@
 % c = [85,0];
 % V = 55000;
 % Sigma = [2000,0;0,2000];
-% alpha = 1.8;
+% alpha = 0.85;
 
 function H = unit_sym_input_2d(X,Y,c,V,Sigma,alpha)
 
@@ -29,15 +29,43 @@ Xpos = X(:,I+1:end); Xneg = X(:,1:I);
 Ypos = Y(:,I+1:end); Yneg = Y(:,1:I);
 
 lambdaX = Sigma(1,1); lambdaY = Sigma(2,2);
+KK = V/(2*pi*det(Sigma)^0.5);
 
-Hpos = V * inv(2*pi*det(Sigma)^0.5)*exp(-0.5*(inv(lambdaX)*(alpha*(Xpos-xc)).^2 + ...
+Hpos = KK*exp(-0.5*(inv(lambdaX)*(alpha*(Xpos-xc)).^2 + ...
     inv(lambdaY)*(Ypos-yc).^2));
-Hneg = V * inv(2*pi*det(Sigma)^0.5)*exp(-0.5*(inv(lambdaX)*(alpha/(2*alpha-1)*(Xneg-xc)).^2 + ...
+Hneg = KK*exp(-0.5*(inv(lambdaX)*(alpha/(2*alpha-1)*(Xneg-xc)).^2 + ...
     inv(lambdaY)*(Yneg-yc).^2));
-H = [Hneg,Hpos];
 
+% % wall effect
+% xf = 170; yr = -80; yl = 80; % the position of walls
+% 
+% Hpos = Hpos + KK *exp(-0.5*(inv(lambdaX)*(alpha*(Xpos-xc)).^2 + ...
+%     inv(lambdaY)*(-(Ypos-(2*yl-yc))).^2)); % yl - pos
+% Hneg = Hneg + KK*exp(-0.5*(inv(lambdaX)*(alpha/(2*alpha-1)*(Xneg-xc)).^2 + ...
+%     inv(lambdaY)*(-(Yneg-(2*yl-yc))).^2)); % yl - neg
+% 
+% Hpos = Hpos + KK*exp(-0.5*(inv(lambdaX)*(alpha*(Xpos-xc)).^2 + ...
+%     inv(lambdaY)*(-(Ypos-(2*yr-yc))).^2)); % yr - pos
+% Hneg = Hneg + KK*exp(-0.5*(inv(lambdaX)*(alpha/(2*alpha-1)*(Xneg-xc)).^2 + ...
+%     inv(lambdaY)*(-(Yneg-(2*yr-yc))).^2)); % yr - neg
+% 
+% Hneg = Hneg + KK*exp(-0.5*(inv(lambdaX)*(alpha*(-(Xneg-(2*xf-xc)))).^2 + ...
+%     inv(lambdaY)*(Yneg-yc).^2)); % xf - neg
+% Hneg = Hneg + KK*exp(-0.5*(inv(lambdaX)*(alpha*(-(Xneg-(2*xf-xc)))).^2 + ...
+%     inv(lambdaY)*(-(Yneg-(2*yl-yc))).^2)); % xf & yl - neg
+% Hneg = Hneg + KK*exp(-0.5*(inv(lambdaX)*(alpha*(-(Xneg-(2*xf-xc)))).^2 + ...
+%     inv(lambdaY)*(-(Yneg-(2*yr-yc))).^2)); % xf & yr - neg
+% 
+% Hpos = Hpos + KK*exp(-0.5*(inv(lambdaX)*(alpha*(-(Xpos-(2*xf-xc)))).^2 + ...
+%     inv(lambdaY)*(Ypos-yc).^2)); % xf - pos
+% Hpos = Hpos + KK*exp(-0.5*(inv(lambdaX)*(alpha*(-(Xpos-(2*xf-xc)))).^2 + ...
+%     inv(lambdaY)*(-(Ypos-(2*yl-yc))).^2)); % xf & yl - pos
+% Hpos = Hpos + KK*exp(-0.5*(inv(lambdaX)*(alpha*(-(Xpos-(2*xf-xc)))).^2 + ...
+%     inv(lambdaY)*(-(Ypos-(2*yr-yc))).^2)); % xf & yr - pos
+
+H = [Hneg,Hpos];
 end
 
 % % [test] plot
 % X = [Xneg,Xpos]; Y = [Yneg,Ypos];
-% figure;mesh(X,Y,H)
+% figure;mesh(X,Y,H); xlim([0 170]);
